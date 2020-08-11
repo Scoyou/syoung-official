1ee4433b0e4bd46cb8ba1538d17a0381cf35236b
Scott Young <syoung@instructure.com>
2020-08-11 11:22:26 -0600

    implement carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+
+import Card from '../components/Card';
+
+import placeholder from '../assets/images/placeholder.png';
+
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: 'Item 1',
+                    subTitle: 'subtitle',
+                    imgSrc: placeholder,
+                    link: '',
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: 'Item 2',
+                    subTitle: 'subtitle',
+                    imgSrc: placeholder,
+                    link: '',
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: 'Item 3',
+                    subTitle: 'subtitle',
+                    imgSrc: placeholder,
+                    link: '',
+                    selected: false
+                },
+            ]
+        }
+    }
+
+
+    handleCardClick = (id, card) => {
+
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return (
+                <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+            )
+        })
+    }
+
+
+    render() {
+        return(
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
+        );
+    }
+
+}
+
+export default Carousel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Hero from '../components/Hero'
+import Carousel from '../components/Carousel'
 
 const Home = (props) => (
   <div>
       <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
-      <p>From Home</p>
+      <Carousel />
   </div>
 )
 
