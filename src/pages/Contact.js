e47fb5d7843985f073a0a91dfc5f33df5d985372
Scott Young <syoung@instructure.com>
2020-08-13 12:33:38 -0600

    change contact path to use proxy

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -35,7 +35,7 @@ class Contact extends Component {
       disabled: true
     })
 
-    axios.post('http://localhost:3030/api/email', this.state)
+    axios.post('/api/email', this.state)
     .then(res => {
         if(res.data.success) {
             this.setState({
