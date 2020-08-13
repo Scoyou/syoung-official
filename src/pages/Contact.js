ee4c9f6616eb54c35d635d002c3ab42c2fe245f1
Scott Young <syoung@instructure.com>
2020-08-13 12:31:06 -0600

    server working

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 
 import Hero from '../components/Hero';
 import Content from '../components/Content';
+import axios from 'axios';
 
 
 class Contact extends Component {
@@ -32,6 +33,29 @@ class Contact extends Component {
 
     this.setState({
       disabled: true
+    })
+
+    axios.post('http://localhost:3030/api/email', this.state)
+    .then(res => {
+        if(res.data.success) {
+            this.setState({
+                disabled: false,
+                emailSent: true
+            });
+        } else {
+            this.setState({
+                disabled: false,
+                emailSent: false
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+
+        this.setState({
+            disabled: false,
+            emailSent: false
+        });
     })
   }
 
